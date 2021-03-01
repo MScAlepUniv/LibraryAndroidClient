3c830005f185637f586c57410ad8e59937b70b47
Jamil <jamil.abodan@gmail.com>
2021-03-01 15:37:48 +0200

    add a new variable

--- src/app/src/main/java/com/alepuniv/library/MainActivity.java
+++ src/app/src/main/java/com/alepuniv/library/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+     ptivate int myVariable;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
