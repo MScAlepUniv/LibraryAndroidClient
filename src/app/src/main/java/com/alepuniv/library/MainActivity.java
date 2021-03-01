0b7bc166df7dbccba6c933b983cb292b19407799
Yaman Kalaji <syk.yaman@gmail.com>
2021-03-01 15:48:17 +0200

    Re-change varible name again

--- src/app/src/main/java/com/alepuniv/library/MainActivity.java
+++ src/app/src/main/java/com/alepuniv/library/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-     ptivate int myVariable;
+     ptivate int ourVariable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
