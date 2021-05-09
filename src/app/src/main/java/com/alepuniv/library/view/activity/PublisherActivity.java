5c9ff208d2174befe545342370c838a2d11bdff9
Jamil <jamil.abodan@gmail.com>
2021-05-09 15:49:45 +0300

    Published and Author

---
+++ src/app/src/main/java/com/alepuniv/library/view/activity/PublisherActivity.java
@@ -0,0 +1,16 @@
+package com.alepuniv.library.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alepuniv.library.R;
+
+public class PublisherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publisher);
+    }
+}
