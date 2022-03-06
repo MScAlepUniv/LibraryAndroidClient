62f1cc519ebd919aa7172b2ba32ea4fdce74fbd8
Yaman Kalaji <syk.yaman@gmail.com>
2022-03-06 04:20:24 +0200

    Implementing the new API inside the Author activity (GET an author with an Id = 1) Note: the token is kept inside the activity for testing purposes, please don't judge.

--- src/app/src/main/java/com/alepuniv/library/AuthorActivity.java
+++ src/app/src/main/java/com/alepuniv/library/AuthorActivity.java
@@ -1,14 +1,34 @@
 package com.alepuniv.library;
 
 import androidx.appcompat.app.AppCompatActivity;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 import android.os.Bundle;
 
+import com.alepuniv.library.api.LibraryAPI;
+import com.alepuniv.library.api.RetrofitHelper;
+import com.alepuniv.library.model.entity.Author;
+
+import java.io.IOException;
+
 public class AuthorActivity extends AppCompatActivity {
+
+    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19hZG1pbiI6ImZhbHNlIiwiaWQiOiIxIiwiaWF0IjoxNjQ2NTMzMDgxfQ.CBukzUtzpovRxG4pnRpj_3VWQ1EJhEz8_4eK1a94vyI";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_author);
+
+
+        Call<Author> call = RetrofitHelper.getLibraryAPI(token).getAuthor(1);
+        try {
+            Author authorResult = call.execute().body();
+            //Show authorResult on the activity
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
