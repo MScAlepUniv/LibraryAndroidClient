39ec6f70b0bae2d3d594a7ec2d90e961cccbf8b6
Yaman Kalaji <syk.yaman@gmail.com>
2022-03-06 04:17:36 +0200

    Add a model for the author to be used in the API call

---
+++ src/app/src/main/java/com/alepuniv/library/api/LibraryAPI.java
@@ -0,0 +1,14 @@
+package com.alepuniv.library.api;
+
+import com.alepuniv.library.model.entity.*;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface LibraryAPI {
+
+    @GET("/mysql/common/getAuthor/{id}")
+    Call<Author> getAuthor(@Path("id") int id);
+
+}
---
+++ src/app/src/main/java/com/alepuniv/library/model/entity/Author.java
@@ -0,0 +1,13 @@
+package com.alepuniv.library.model.entity;
+
+import java.util.Date;
+
+public class Author {
+    public String name;
+    public String last_name;
+    public String birthday;
+    public String place_of_birth;
+    public String address;
+    public Date date_of_death;
+    public String biography;
+}
